9815adf1ad4096d2b46c8d0fb5e00ad650ad0cbd
Mirashvili-Anastasia <a1111.1mira@gmail.com>
2026-02-16 19:36:28 +0300

    lab1_Commit

--- lab1/00_distance.py
+++ lab1/00_distance.py
@@ -12,29 +12,29 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-distances = []
+distances = {}
 
-Moscow_distances_London = round(((sites['Moscow'][0] - sites['London'][0]) ** 2 + (sites['Moscow'][1] - sites['London'][1] ** 2)) ** 0.5)
-Moscow_distances_Paris = round(((sites['Moscow'][0] - sites['Paris'][0]) ** 2 + (sites['Moscow'][1] - sites['Paris'][1] ** 2)) ** 0.5)
+Moscow_distances_London = round(((sites['Moscow'][0] - sites['London'][0]) ** 2 + (sites['Moscow'][1] - sites['London'][1]) ** 2) ** 0.5, 2)
+Moscow_distances_Paris = round(((sites['Moscow'][0] - sites['Paris'][0]) ** 2 + (sites['Moscow'][1] - sites['Paris'][1]) ** 2) ** 0.5, 2)
 
-London_distances_Moscow = round(((sites['London'][0] - sites['Moscow'][0]) ** 2 + (sites['London'][1] - sites['Moscow'][1] ** 2)) ** 0.5)
-London_distances_Paris = round(((sites['London'][0] - sites['Paris'][0]) ** 2 + (sites['London'][1] - sites['Paris'][1] ** 2)) ** 0.5)
+London_distances_Moscow = round(((sites['London'][0] - sites['Moscow'][0]) ** 2 + (sites['London'][1] - sites['Moscow'][1]) ** 2) ** 0.5, 2)
+London_distances_Paris = round(((sites['London'][0] - sites['Paris'][0]) ** 2 + (sites['London'][1] - sites['Paris'][1]) ** 2) ** 0.5, 2)
 
-Paris_distances_Moscow = round(((sites['Paris'][0] - sites['Moscow'][0]) ** 2 + (sites['Paris'][1] - sites['Moscow'][1] ** 2)) ** 0.5)
-Paris_distances_London = round(((sites['Paris'][0] - sites['London'][0]) ** 2 + (sites['Paris'][1] - sites['London'][1] ** 2)) ** 0.5)
+Paris_distances_Moscow = round(((sites['Paris'][0] - sites['Moscow'][0]) ** 2 + (sites['Paris'][1] - sites['Moscow'][1]) ** 2) ** 0.5, 2)
+Paris_distances_London = round(((sites['Paris'][0] - sites['London'][0]) ** 2 + (sites['Paris'][1] - sites['London'][1]) ** 2) ** 0.5, 2)
 
 distances['Moscow'] = {
-    'London': Moscow_distances_London
-    'Paris': Moscow_distances_Paris
+    'London': Moscow_distances_London,
+    'Paris': Moscow_distances_Paris,
 }
 
 distances['London'] = {
-    'Moscow': London_distances_Moscow
+    'Moscow': London_distances_Moscow,
     'Paris': London_distances_Paris
 }
 
 distances['Paris'] = {
-    'London': Paris_distances_Londo
+    'London': Paris_distances_London,
     'Moscow': Paris_distances_Moscow
 }
 
