b5cf37dbddc8b825127a85a1ceb3c303b3c35eef
Mirashvili-Anastasia <a1111.1mira@gmail.com>
2026-02-19 19:56:21 +0300

    lab1_Commit

--- lab1/07_secret.py
+++ lab1/07_secret.py
@@ -33,3 +33,12 @@ secret_message = [
 #   Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
 # TODO вывести расшифрованное сообщение
+first_word = secret_message[0][3]
+second_word = secret_message[1][9:13]
+third_word = secret_message[2][5:15:2]
+fourth_word = secret_message[3][12:6:-1]
+fifth_word = secret_message[4][20:15]
+
+secret_message = f'{first_word} {second_word} {third_word} {fourth_word} {fifth_word}'
+print(secret_message)
+
